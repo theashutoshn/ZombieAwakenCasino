e3511e01e1aa0c55ba8d2f7af2d12897d4c5cbdb
AshutoshNandanwar <95475646+theashutoshn@users.noreply.github.com>
2023-12-12 22:37:42 +0530

    Gun Movement Based on Mouse Position

--- Assets/Scripts/GunMechanism.cs
+++ Assets/Scripts/GunMechanism.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GunMechanism : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _speed = 0.5f;
     void Start()
     {
         
@@ -13,6 +13,8 @@ public class GunMechanism : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+
+        transform.position = Vector3.MoveTowards(transform.position, mousePos, _speed * Time.deltaTime);
     }
 }
